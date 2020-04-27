51d3bcb6069f4448e7ce7b1dbfbf2f9f9f2959c5
Rachel Selby <rachel.b.selby-1@ou.edu>
2020-04-27 12:52:48 -0500

    map of devon complete

--- src/MajorProject/go_to_point.py
+++ src/MajorProject/go_to_point.py
@@ -75,7 +75,12 @@ if __name__ == '__main__':
         navigator = GoToPose()
 
         # Customize the following values so they are appropriate for your location
-        position = {'x': -0.507, 'y' : -2.02}
+        # Atrium
+        # Computer Lab
+        # Electronics Lab
+        # Team Room
+        # Main Office
+        position = {'x': 1.57, 'y' : -6.00}
         quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
 
         rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
