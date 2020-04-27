601da347f0265e0556a62a2cee2dcd5f1d11fe8a
Rachel Selby <rachel.b.selby-1@ou.edu>
2020-04-27 15:54:59 -0500

    add executables for go_to_point and user_input

--- src/MajorProject/go_to_point.py
+++ src/MajorProject/go_to_point.py
@@ -10,18 +10,18 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
-# TurtleBot must have minimal.launch & amcl_demo.launch
-# running prior to starting this script
-# For simulation: launch gazebo world & amcl_demo prior to run this script
+# This code is adapted from Mark Silliman's go_to_specific_point_on_map.py https://github.com/markwsilliman/turtlebot/blob/master/go_to_specific_point_on_map.py
 
 import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
 from geometry_msgs.msg import Pose, Point, Quaternion
+from std_msgs.msg import Bool
 
 class GoToPose():
     def __init__(self):
+        rospy.init_node('go_to_point', anonymous=False)
 
         self.goal_sent = False
 
@@ -35,15 +35,22 @@ class GoToPose():
 	# Allow up to 5 seconds for the action server to come up
 	self.move_base.wait_for_server(rospy.Duration(5))
 
-    def goto(self, pos, quat):
+        self.sub = rospy.Subscriber('user_input', Point, self.callback)
+        #self.pub = rospy.Publisher('nav_result', Bool, queue_size=10)
 
+	# Spin until the node is terminated
+        rospy.spin()
+
+
+    def callback(self, data):
         # Send a goal
         self.goal_sent = True
 	goal = MoveBaseGoal()
 	goal.target_pose.header.frame_id = 'map'
 	goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
-                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
+
+        goal.target_pose.pose = Pose(Point(data.x, data.y, 0.000),
+                                     Quaternion(0.000, 0.000, 0.000, 1.000))
 
 	# Start moving
         self.move_base.send_goal(goal)
@@ -61,7 +68,17 @@ class GoToPose():
             self.move_base.cancel_goal()
 
         self.goal_sent = False
-        return result
+
+        # Tell user_input node we've arrived at the destination
+        if success:
+            rospy.loginfo("We've arrived at the desired destination")
+        else:
+            rospy.loginfo("The base failed to the desired destination")
+
+        #self.pub.publish(success)
+       
+        # Sleep to give the last log messages time to be sent
+        rospy.sleep(1)
 
     def shutdown(self):
         if self.goal_sent:
@@ -69,30 +86,9 @@ class GoToPose():
         rospy.loginfo("Stop")
         rospy.sleep(1)
 
+
 if __name__ == '__main__':
     try:
-        rospy.init_node('nav_test', anonymous=False)
-        navigator = GoToPose()
-
-        # Customize the following values so they are appropriate for your location
-        # Atrium
-        # Computer Lab
-        # Electronics Lab
-        # Team Room
-        # Main Office
-        position = {'x': 1.57, 'y' : -6.00}
-        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
-
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        success = navigator.goto(position, quaternion)
-
-        if success:
-            rospy.loginfo("Hooray, reached the desired pose")
-        else:
-            rospy.loginfo("The base failed to reach the desired pose")
-
-        # Sleep to give the last log messages time to be sent
-        rospy.sleep(1)
-
+        GoToPose()
     except rospy.ROSInterruptException:
-        rospy.loginfo("Ctrl-C caught. Quitting")
+        rospy.loginfo("go_to_point node terminated")
