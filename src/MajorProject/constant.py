6730a86229fc3c45ae3bdc797ff53d0363a7419e
Rachel Selby <rachel.b.selby-1@ou.edu>
2020-05-04 01:50:46 -0500

    add checking if already in location

--- src/MajorProject/constant.py
+++ src/MajorProject/constant.py
@@ -17,6 +17,13 @@ ELEC_LAB_POS = Point(1, -1, 0)
 TEAM_ROOM_POS = Point(-8, -6, 0)
 MAIN_OFFICE_POS = Point(-8, 3, 0)
 
+# Highlight locations bounding points
+ATRIUM_BOUNDS = (Point(-3.67, -7.06, 0), Point(9.62, -2.52, 0))
+COMP_LAB_BOUNDS = (Point(5.47, -2.46, 0), Point(9.96, 1.05, 0))
+ELEC_LAB_BOUNDS = (Point(-3.41, -2.96, 0), Point(5.26, 0.802, 0))
+TEAM_ROOM_BOUNDS = (Point(-9.92, -6.99, 0), Point(-6.53, -4.47, 0))
+MAIN_OFFICE_BOUNDS = (Point(-9.95, 0.514, 0), Point(-6.6, 4.86, 0))
+
 # Dictionary of locations
 LOCATIONS = {ATRIUM_NAME: ATRIUM_POS, COMP_LAB_NAME : COMP_LAB_POS, ELEC_LAB_NAME: ELEC_LAB_POS, 
                               TEAM_ROOM_NAME: TEAM_ROOM_POS, MAIN_OFFICE_NAME: MAIN_OFFICE_POS}
